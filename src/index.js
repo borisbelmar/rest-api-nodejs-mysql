f20e961d62274cfdcd082f72a4b5f5aa00db8e8d
Boris Belmar <borisbelmarm@gmail.com>
2019-05-01 21:33:27 -0400

    Primer Commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+// Settings
+// Sección de configuración de la aplicación
+// Puerto, Nombre, Motor de plantillas, etc...
+
+
+
+// Middleware
+// Funciones que se ejecutan antes de llegar a las rutas
+
+
+
+// Rutas
+// Crear URLs para procesar datos
+
+
+
+// Starting de Server
+
+app.listen(3000, () => {
+    console.log('Server on port 3000');
+});
