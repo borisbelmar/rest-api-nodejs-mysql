c5018de50f0611fd583f37d13c08cf1c392ca780
Boris Belmar <borisbelmarm@gmail.com>
2019-05-03 00:15:09 -0400

    Cambios en la base de datos

--- src/database.js
+++ src/database.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'boris',
-    password: 'password',
+    password: 'boris',
     database: 'company'
 });
 
