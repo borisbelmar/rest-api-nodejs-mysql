fb32065919a3e065b84e165df2d844983540e660
Boris Belmar <borisbelmarm@gmail.com>
2019-05-05 20:43:06 -0400

    Cambios en la ruta employees

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -6,10 +6,23 @@ const router = express.Router();
 
 const mysqlConnection = require('../database');
 
+// API REST para consultar todos los registros en employee
 router.get('/', (req, res) => {
-    mysqlConnection.query('SELECT * FROM employees', (err, rows, fields) => {
+    mysqlConnection.query('SELECT * FROM employee', (err, rows, fields) => {
         if(!err) {
             res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('SELECT * FROM employee WHERE id = ?', [id] ,(err, rows, fields) => {
+        if(!err) {
+            res.json(rows[0]);
         } else {
             console.log(err);
         }
